3e3fb81d487672494f5e7c439c9145040b1502fb
Leon Frenot <leon.a.frenot@inria.fr>
2026-09-16 09:48:38 +0200

    Experiment convert script

---
+++ experiment_schedules/convert-results.py
@@ -0,0 +1,50 @@
+import argparse
+import os
+import re
+import csv
+import json
+
+INPUT_R = re.compile(r"(.*)-(.*)\.yml\.csv")
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument("input", type=str, help="Path to the folder containing results")
+    # parser.add_argument(
+    #     "--output",
+    #     type=str,
+    #     help="Path to the output folder"
+    # )
+    parser.add_argument("-c", type=int, default=1, help="number of cores")
+
+    args = parser.parse_args()
+
+    input = args.input
+    cores = args.c
+
+    output = input + "/results_json/"
+
+    if not os.path.isdir(output):
+        os.mkdir(output)
+
+    for f in os.listdir(input):
+        if f.endswith(".csv"):
+            m = INPUT_R.match(f)
+            if not m:
+                print(f"WARNING: file {f} does not match regex.")
+                continue
+            op, sched = m.groups()
+            op = op.lower()
+            out_f = f"{output}/results.{op}.{sched}.2048.{cores}.1.jsonl"
+            with open(f"{input}/{f}", "r") as f_in, open(out_f, "w") as f_out:
+                data_in = csv.DictReader(f_in)
+                data_out = [{"results": [float(r["time"])]} for r in data_in]
+                print(data_out)
+                for result in data_out:
+                    json.dump(result, f_out)
+
+
+if __name__ == "__main__":
+    main()
